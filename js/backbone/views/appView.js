fe1457496f901716f0d99e07795c647cc9676efa
anniebae <bae.annie@gmail.com>
2015-03-12 17:46:16 -0400

    triggerAnimation function created to render animations on click of menu-item

--- js/backbone/views/appView.js
+++ js/backbone/views/appView.js
@@ -11,7 +11,8 @@ var AppView = Backbone.View.extend({
     'click #home-tab'       : 'renderHome',
     'click #morning-tab'    : 'renderMorning',
     'click #afternoon-tab'  : 'renderAfternoon',
-    'click #evening-tab'     : 'renderEvening'
+    'click #evening-tab'    : 'renderEvening',
+    'click .menu-item'      : 'triggerAnimation'
   },
   renderHome: function() {
     $('#body').html(this.homeTemplate());
@@ -23,8 +24,31 @@ var AppView = Backbone.View.extend({
   },
   renderAfternoon: function() {
     $('#body').html(this.afternoonTemplate());
+    return this;
   },
   renderEvening: function() {
     $('#body').html(this.eveningTemplate());
+    return this;
+  },
+  triggerAnimation: function() {
+    if ($('html').hasClass('no-js'))
+      $('html').toggleClass('no-js js');
+      if ($(window).width() <= 568) {
+        $('.animate-in').removeClass('animate-in animating animate-out infinite').removeClass(effects.join(' '));
+      } else {
+        // Animate element
+        $('.animate-in').each(function(i, elem) {
+          var type = $(elem).attr('data-anim-type'),
+          delay = $(elem).attr('data-anim-delay');
+          $(elem).appear(function () {
+            setTimeout(function() {
+            $(elem).addClass('animating').addClass(type).removeClass('animate-in');
+            }, delay);
+            $(elem).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                $(elem).removeClass('animating').removeClass(effects.join(' '));
+            });
+          }, { accX: 0, accY: -100 });
+        });
+      }
   },
 });
