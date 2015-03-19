8e06e163797c15b8af778bfe00c5d2fde19bc406
Evan Turner <evturn@gmail.com>
2015-03-19 04:29:50 -0400

    iterating over a collection inside template

--- js/views/timeline.view.js
+++ js/views/timeline.view.js
@@ -1,3 +1,17 @@
 var TimelineView = Backbone.View.extend({
-	
+	el: '#timeline-view',
+	timelineTemplate: _.template($('#timeline-template').html()),
+	initialize: function() {
+		this.render();
+	},
+	morning: function() {
+		this.$el.html(timelineTemplate());
+		return this;
+	},
+	afternoon: function() {
+
+	},
+	evening: function() {
+
+	},
 });
