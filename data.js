ff95a5078421390404c94e076d3999225a1ef99a
anniebae <bae.annie@gmail.com>
2015-03-22 23:20:20 -0400

    caption for each hour written

--- data.js
+++ data.js
@@ -17,7 +17,7 @@ timeline1 = [
   },
   {
     time: '10am',
-    desc: 'Drown in coffee and code for hours.',
+    desc: 'Drown in coffee, check emails, code for hours.',
     image: 'css/images/7.png'
   },
   {
@@ -36,32 +36,32 @@ timeline2 = [
   {day: "afternoon"},
   {
     time: '1pm',
-    desc: 'Lorem ipsum dolor sit amet, eros elit pretium at, id similique pede maecenas, lectus imperdiet, sed vestibulum netus augue vitae quam mollis, eget tortor euismod ultrices hendrerit. Id mauris libero natoque nibh vestibulum vel. Quis aliquam quis, proin et nascetur eget vestibulum.',
+    desc: 'Contemplate on getting lunch.',
     image: 'css/images/1.png'
   },
   {
     time: '2pm',
-    desc: 'Lorem ipsum dolor sit amet, eros elit pretium at, id similique pede maecenas, lectus imperdiet, sed vestibulum netus augue vitae quam mollis, eget tortor euismod ultrices hendrerit. Id mauris libero natoque nibh vestibulum vel. Quis aliquam quis, proin et nascetur eget vestibulum.',
+    desc: 'Give in and stuff my face.',
     image: 'css/images/2.png'
   },
   {
     time: '3pm',
-    desc: 'Lorem ipsum dolor sit amet, eros elit pretium at, id similique pede maecenas, lectus imperdiet, sed vestibulum netus augue vitae quam mollis, eget tortor euismod ultrices hendrerit. Id mauris libero natoque nibh vestibulum vel. Quis aliquam quis, proin et nascetur eget vestibulum.',
+    desc: 'Go for a walk.',
     image: 'css/images/3.png'
   },
   {
     time: '4pm',
-    desc: 'Lorem ipsum dolor sit amet, eros elit pretium at, id similique pede maecenas, lectus imperdiet, sed vestibulum netus augue vitae quam mollis, eget tortor euismod ultrices hendrerit. Id mauris libero natoque nibh vestibulum vel. Quis aliquam quis, proin et nascetur eget vestibulum.',
+    desc: 'Check on more emails, drink more coffee.',
     image: 'css/images/4.png'
   },
   {
     time: '5pm',
-    desc: 'Lorem ipsum dolor sit amet, eros elit pretium at, id similique pede maecenas, lectus imperdiet, sed vestibulum netus augue vitae quam mollis, eget tortor euismod ultrices hendrerit. Id mauris libero natoque nibh vestibulum vel. Quis aliquam quis, proin et nascetur eget vestibulum.',
+    desc: 'Wrap up on current project.',
     image: 'css/images/5.png'
   },
   {
     time: '6pm',
-    desc: 'Lorem ipsum dolor sit amet, eros elit pretium at, id similique pede maecenas, lectus imperdiet, sed vestibulum netus augue vitae quam mollis, eget tortor euismod ultrices hendrerit. Id mauris libero natoque nibh vestibulum vel. Quis aliquam quis, proin et nascetur eget vestibulum.',
+    desc: 'Find a bug in project, get stuck in dilemma.',
     image: 'css/images/6.png'
   }
 ];
@@ -70,32 +70,32 @@ timeline3 = [
   {day: "evening"},
   {
     time: '7pm',
-    desc: 'Lorem ipsum dolor sit amet, eros elit pretium at, id similique pede maecenas, lectus imperdiet, sed vestibulum netus augue vitae quam mollis, eget tortor euismod ultrices hendrerit. Id mauris libero natoque nibh vestibulum vel. Quis aliquam quis, proin et nascetur eget vestibulum.',
+    desc: 'Fix bug or save for tomorrow.',
     image: 'css/images/1.png'
   },
   {
     time: '8pm',
-    desc: 'Lorem ipsum dolor sit amet, eros elit pretium at, id similique pede maecenas, lectus imperdiet, sed vestibulum netus augue vitae quam mollis, eget tortor euismod ultrices hendrerit. Id mauris libero natoque nibh vestibulum vel. Quis aliquam quis, proin et nascetur eget vestibulum.',
+    desc: 'Pass out on bus.',
     image: 'css/images/5.png'
   },
   {
     time: '9pm',
-    desc: 'Lorem ipsum dolor sit amet, eros elit pretium at, id similique pede maecenas, lectus imperdiet, sed vestibulum netus augue vitae quam mollis, eget tortor euismod ultrices hendrerit. Id mauris libero natoque nibh vestibulum vel. Quis aliquam quis, proin et nascetur eget vestibulum.',
+    desc: 'Shove my face in car.',
     image: 'css/images/6.png'
   },
   {
     time: '10pm',
-    desc: 'Lorem ipsum dolor sit amet, eros elit pretium at, id similique pede maecenas, lectus imperdiet, sed vestibulum netus augue vitae quam mollis, eget tortor euismod ultrices hendrerit. Id mauris libero natoque nibh vestibulum vel. Quis aliquam quis, proin et nascetur eget vestibulum.',
+    desc: 'Sneak in a snack if ambitious. ',
     image: 'css/images/7.png'
   },
   {
     time: '11pm',
-    desc: 'Lorem ipsum dolor sit amet, eros elit pretium at, id similique pede maecenas, lectus imperdiet, sed vestibulum netus augue vitae quam mollis, eget tortor euismod ultrices hendrerit. Id mauris libero natoque nibh vestibulum vel. Quis aliquam quis, proin et nascetur eget vestibulum.',
+    desc: "Wash up, read Tiny Fey's Bossypants",
     image: 'css/images/8.png'
   },
   {
     time: '12pm',
-    desc: 'Lorem ipsum dolor sit amet, eros elit pretium at, id similique pede maecenas, lectus imperdiet, sed vestibulum netus augue vitae quam mollis, eget tortor euismod ultrices hendrerit. Id mauris libero natoque nibh vestibulum vel. Quis aliquam quis, proin et nascetur eget vestibulum.',
+    desc: 'Go to bed, try not to drool on pillow.',
     image: 'css/images/9.png'
   }
 ];
